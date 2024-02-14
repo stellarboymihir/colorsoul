db3f88c50b3a0d72d853fbd4d0e179bbdeb6f0e0
Mihir Bhadange <83822717+stellarboymihir@users.noreply.github.com>
2024-02-14 13:50:18 +0530

    Till lunch

--- lib/dashboard/distributor/filter.dart
+++ lib/dashboard/distributor/filter.dart
@@ -14,6 +14,7 @@ class _FilterState extends State<Filter> {
   TextEditingController searchController = TextEditingController();
   var onPressed = 'state';
   List<bool> onClickState = [false, false, false];
+  double _currSliderValue = 0;
 
   final List<Map<String, dynamic>> stateList = [
     {
@@ -23,32 +24,197 @@ class _FilterState extends State<Filter> {
     {
       "title": 'Arunachal Pradesh',
       'onPressed': false,
-    },{
-      "title": 'Arunachal Pradesh',
-      'onPressed': false,
-    },{
+    },
+    {
       "title": 'Assam',
       'onPressed': false,
-    },{
+    },
+    {
       "title": 'Bihar',
       'onPressed': false,
-    },{
+    },
+    {
       "title": 'Chhattisgarh',
       'onPressed': false,
-    },{
+    },
+    {
       "title": 'Goa',
       'onPressed': false,
-    },{
+    },
+    {
       "title": 'Gujarat	',
       'onPressed': false,
-    },{
+    },
+    {
       "title": 'Haryana	',
       'onPressed': false,
-    },{
+    },
+    {
       "title": 'Himachal Pradesh',
       'onPressed': false,
-    },{
+    },
+    {
       "title": 'Jharkhand',
+      'onPressed': false,
+    },
+  ];
+  final List<Map<String, dynamic>> regionList = [
+    {
+      "title": 'South',
+      'onPressed': false,
+    },
+    {
+      "title": 'Central',
+      'onPressed': false,
+    },
+    {
+      "title": 'North',
+      'onPressed': false,
+    },
+    {
+      "title": 'Saurashtra',
+      'onPressed': false,
+    },
+    {
+      "title": 'Kutch',
+      'onPressed': false,
+    },
+  ];
+  final List<Map<String, dynamic>> cityList = [
+    {
+      "title": 'Surat',
+      'onPressed': false,
+    },
+    {
+      "title": 'Bharuch',
+      'onPressed': false,
+    },
+    {
+      "title": 'Narmada',
+      'onPressed': false,
+    },
+    {
+      "title": 'Navsari',
+      'onPressed': false,
+    },
+    {
+      "title": 'Dang',
+      'onPressed': false,
+    },
+    {
+      "title": 'Valsad',
+      'onPressed': false,
+    },
+    {
+      "title": 'Tapi',
+      'onPressed': false,
+    },
+    {
+      "title": 'Ahmedabad',
+      'onPressed': false,
+    },
+    {
+      "title": 'GandhiNagar',
+      'onPressed': false,
+    },
+    {
+      "title": 'Rajkot',
+      'onPressed': false,
+    },
+    {
+      "title": 'Vadodara',
+      'onPressed': false,
+    },
+  ];
+  final List<Map<String, dynamic>> areaList = [
+    {
+      "title": 'Althan',
+      'onPressed': false,
+    },
+    {
+      "title": 'Mandvi',
+      'onPressed': false,
+    },
+    {
+      "title": 'Bardoli',
+      'onPressed': false,
+    },
+    {
+      "title": 'Palsana',
+      'onPressed': false,
+    },
+    {
+      "title": 'Mahuva',
+      'onPressed': false,
+    },
+    {
+      "title": 'Kamrej',
+      'onPressed': false,
+    },
+    {
+      "title": 'Mangrol',
+      'onPressed': false,
+    },
+    {
+      "title": 'Choryasi',
+      'onPressed': false,
+    },
+    {
+      "title": 'Olpad',
+      'onPressed': false,
+    },
+    {
+      "title": 'Umarpada',
+      'onPressed': false,
+    },
+    {
+      "title": 'Adajan',
+      'onPressed': false,
+    },
+    {
+      "title": 'Katargam',
+      'onPressed': false,
+    },
+    {
+      "title": 'Godadara',
+      'onPressed': false,
+    },
+    {
+      "title": 'Athwalines',
+      'onPressed': false,
+    },
+    {
+      "title": 'Bhagal',
+      'onPressed': false,
+    },
+    {
+      "title": 'Gopipura',
+      'onPressed': false,
+    },
+    {
+      "title": 'Limbayat',
+      'onPressed': false,
+    },
+  ];
+  final List<Map<String, dynamic>> radiusList = [
+    {
+      "title": '5 Km',
+      'onPressed': false,
+    },
+    {
+      "title": '10 Km',
+      'onPressed': false,
+    },
+    {
+      "title": '15 Km',
+      'onPressed': false,
+    },
+    {
+      "title": '20 Km',
+      'onPressed': false,
+    },
+    {
+      "title": 'Custom',
       'onPressed': false,
     },
   ];
@@ -223,7 +389,7 @@ class _FilterState extends State<Filter> {
             children: [
               Container(
                 width: MediaQuery.of(context).size.width * 0.55,
-                margin: EdgeInsets.only(left: 11),
+                margin: const EdgeInsets.only(left: 11),
                 child: TextFormField(
                   controller: searchController,
                   decoration: InputDecoration(
@@ -258,7 +424,11 @@ class _FilterState extends State<Filter> {
                   keyboardType: TextInputType.text,
                 ),
               ),
-              onPressed == 'state' ? stateFilter() : SizedBox(),
+              onPressed == 'state' ? stateFilter() : const SizedBox(),
+              onPressed == 'region' ? regionFilter() : const SizedBox(),
+              onPressed == 'city' ? cityFilter() : const SizedBox(),
+              onPressed == 'area' ? areaFilter() : const SizedBox(),
+              onPressed == 'radius' ? radiusFilter() : const SizedBox(),
             ],
           )
         ],
@@ -318,23 +488,286 @@ class _FilterState extends State<Filter> {
   }
 
   Widget stateFilter() {
-    return Column(
-      children: [
-        ListView.builder(itemCount: 3, itemBuilder: (context, index) {
-          return Row(
-            children: [
-              Checkbox(value: onClickState[]
-                , onChanged: (bool? val) {
-                setState(() {
-                  onClickState[] = val!;
-                });
-              },),
-              Text('South'),
-            ],
-          )
-        },),
+    return Expanded(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: ListView.builder(
+          padding: EdgeInsets.zero,
+          itemCount: stateList.length,
+          shrinkWrap: true,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Checkbox(
+                  activeColor: MyColor.black,
+                  value: stateList[index]["onPressed"],
+                  onChanged: (bool? val) {
+                    setState(() {
+                      // Unselect all other checkboxes
+                      for (int i = 0; i < stateList.length; i++) {
+                        if (i != index) {
+                          stateList[i]["onPressed"] = false;
+                        }
+                      }
+                      //Select the current checkbox
+                      stateList[index]["onPressed"] = val!;
+                    });
+                  },
+                ),
+                Text(
+                  '${stateList[index]["title"]}',
+                  style: MyStyle.tx14b.copyWith(
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Poppins-Medium",
+                  ),
+                ),
+                const SizedBox(
+                  height: 0,
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
 
-      ],
+  Widget regionFilter() {
+    return Expanded(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: ListView.builder(
+          itemCount: regionList.length,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Checkbox(
+                  activeColor: MyColor.black,
+                  value: regionList[index]["onPressed"],
+                  onChanged: (bool? val) {
+                    setState(() {
+                      // Unselect all other checkboxes
+                      for (int i = 0; i < regionList.length; i++) {
+                        if (i != index) {
+                          regionList[i]["onPressed"] = false;
+                        }
+                      }
+                      //Select the current checkbox
+                      regionList[index]["onPressed"] = val!;
+                    });
+                  },
+                ),
+                Text(
+                  '${regionList[index]["title"]}',
+                  style: MyStyle.tx14b.copyWith(
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Poppins-Medium",
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget cityFilter() {
+    return Expanded(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: ListView.builder(
+          itemCount: cityList.length,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Checkbox(
+                  activeColor: MyColor.black,
+                  value: cityList[index]["onPressed"],
+                  onChanged: (bool? val) {
+                    setState(() {
+                      // Unselect all other checkboxes
+                      for (int i = 0; i < cityList.length; i++) {
+                        if (i != index) {
+                          cityList[i]["onPressed"] = false;
+                        }
+                      }
+                      //Select the current checkbox
+                      cityList[index]["onPressed"] = val!;
+                    });
+                  },
+                ),
+                Text(
+                  '${cityList[index]["title"]}',
+                  style: MyStyle.tx14b.copyWith(
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Poppins-Medium",
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget areaFilter() {
+    return Expanded(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        child: ListView.builder(
+          itemCount: areaList.length,
+          itemBuilder: (context, index) {
+            return Row(
+              children: [
+                Checkbox(
+                  activeColor: MyColor.black,
+                  value: areaList[index]["onPressed"],
+                  onChanged: (bool? val) {
+                    setState(() {
+                      // Unselect all other checkboxes
+                      for (int i = 0; i < areaList.length; i++) {
+                        if (i != index) {
+                          areaList[i]["onPressed"] = false;
+                        }
+                      }
+                      //Select the current checkbox
+                      areaList[index]["onPressed"] = val!;
+                    });
+                  },
+                ),
+                Text(
+                  '${areaList[index]["title"]}',
+                  style: MyStyle.tx14b.copyWith(
+                    fontWeight: FontWeight.w400,
+                    fontFamily: "Poppins-Medium",
+                  ),
+                ),
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+
+  Widget radiusFilter() {
+    return Expanded(
+      child: SizedBox(
+        width: MediaQuery.of(context).size.width * 0.6,
+        // height: MediaQuery.of(context).size.height * 0.6,
+        child: Column(
+          children: [
+            Expanded(
+              child: ListView.builder(
+                itemCount: radiusList.length,
+                itemBuilder: (context, index) {
+                  return Row(
+                    children: [
+                      Checkbox(
+                        activeColor: MyColor.black,
+                        value: radiusList[index]["onPressed"],
+                        onChanged: (bool? val) {
+                          setState(() {
+                            // Unselect all other checkboxes
+                            for (int i = 0; i < radiusList.length; i++) {
+                              if (i != index) {
+                                radiusList[i]["onPressed"] = false;
+                              }
+                            }
+                            //Select the current checkbox
+                            radiusList[index]["onPressed"] = val!;
+                          });
+                        },
+                      ),
+                      Text(
+                        '${radiusList[index]["title"]}',
+                        style: MyStyle.tx14b.copyWith(
+                          fontWeight: FontWeight.w400,
+                          fontFamily: "Poppins-Medium",
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text(
+                    '1km',
+                    style: MyStyle.tx12b.copyWith(
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Poppins-Regular',
+                    ),
+                  ),
+                  Text(
+                    '30km',
+                    style: MyStyle.tx12b.copyWith(
+                      fontWeight: FontWeight.w400,
+                      fontFamily: 'Poppins-Regular',
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 12.0),
+              child: Row(
+                children: [
+                  Container(
+                    height: 28,
+                    width: 28,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: MyColor.black,
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/icons/minus.png',
+                        width: 8,
+                      ),
+                    ),
+                  ),
+                  Expanded(
+                    child: Slider(
+                      label: _currSliderValue.toStringAsFixed(2),
+                      min: 0,
+                      max: 30,
+                      divisions: 10,
+                      activeColor: MyColor.black,
+                      value: _currSliderValue.toDouble(),
+                      onChanged: (double value) {
+                        setState(() {
+                          _currSliderValue = value;
+                        });
+                      },
+                    ),
+                  ),
+                  Container(
+                    height: 28,
+                    width: 28,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: MyColor.black,
+                    ),
+                    child: Center(
+                      child: Image.asset(
+                        'assets/icons/add.png',
+                        width: 8,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
